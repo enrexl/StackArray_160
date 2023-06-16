8ec9d5052d2ad0a79ee627b442f4a4edfdd3d659
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-06-16 10:58:28 +0700

    Membuat pilihan pop / pilihan 2

--- StackArray_160/StackArray_160.cpp
+++ StackArray_160/StackArray_160.cpp
@@ -70,6 +70,15 @@ int main() {
             s.push(element);
             break;
         }
+        case '2': {
+            if (s.empty()) {
+                cout << "\nStack is empty." << endl;
+                break;
+            }
+            s.pop();
+            break;
+        }
+        case '3':
         default:
             break;
         }
