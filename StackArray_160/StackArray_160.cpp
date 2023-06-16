25dfe7b54df27ef15c6376923e5da555a203905d
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-06-16 10:52:48 +0700

    membuat fungsi display

--- StackArray_160/StackArray_160.cpp
+++ StackArray_160/StackArray_160.cpp
@@ -36,6 +36,17 @@ public:
     bool empty() {
         return (top == -1);
     }
+
+    void display() {
+        if (empty()) {
+            cout << "\nStack is empty." << endl;
+        }
+        else {
+            for (int tmp = 0;tmp <= top;tmp++) {
+                cout << stackArray[tmp] << endl;
+            }
+        }
+    }
 };
 
 
